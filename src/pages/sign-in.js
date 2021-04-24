fcdfc1cf49e1f61e842f1e6a6ace34909fe4ff7a
SesH.iO <sesh.io1022@gmail.com>
2021-04-24 22:49:15 +0530

    We added a scroll to top component so it scrolls up when switching pages

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import SignIn from '../components/SignIn/sign-in';
+import ScrollToTop from '../components/UI/scroll-to-top';
 
 const SignInPage = () => {
-	return <SignIn />;
+	return (
+		<Fragment>
+			<ScrollToTop />
+			<SignIn />
+		</Fragment>
+	);
 };
 
 export default SignInPage;
